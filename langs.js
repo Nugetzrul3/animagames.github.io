46c6768c9ab8fadf587237dde2c245e1159b25b3
Mikilis <44727101+Mikilis@users.noreply.github.com>
2020-04-19 13:07:37 +0200

    Update langs.js

--- langs.js
+++ langs.js
@@ -1,8 +1,10 @@
 var languages = {
 	'en': {
-		'lang-alias': 'English'
+		'lang-alias': 'English',
+		'cookies': 'We use cookies to personalise content and ads'
 	},
 	'pl': {
-		'lang-alias': 'Polish'
+		'lang-alias': 'Polish',
+		'cookies': 'Używami plików cookies, aby spersonalizować zawartość strony i reklamy'
 	}
 };
